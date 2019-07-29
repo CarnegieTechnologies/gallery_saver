287ffeae7777174e9c1fa9a37b7341775f6d339d
jelenalecic <jelena.carnegie@gmail.com>
2019-07-29 14:59:08 +0200

    added example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,3 @@
-import 'dart:async';
 import 'dart:io';
 
 import 'package:camera_content_saver/camera_content_saver.dart';
@@ -13,38 +12,74 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    print("flutter start");
-
-    File recodedImage = await ImagePicker.pickImage(source: ImageSource.camera);
-    String platformVersion =
-        await CameraContentSaver.saveImage(recodedImage.path);
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
+  String firstButtonText = 'Take photo';
+  String secondButtonText = 'Record video';
+  double textSize = 20;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        home: Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Column(
+          children: <Widget>[
+            Flexible(
+              flex: 1,
+              child: Container(
+                child: SizedBox.expand(
+                  child: RaisedButton(
+                    color: Colors.blue,
+                    onPressed: _takePhoto,
+                    child: Text(firstButtonText,
+                        style:
+                            TextStyle(fontSize: textSize, color: Colors.white)),
+                  ),
+                ),
+              ),
+            ),
+            Flexible(
+              child: Container(
+                  child: SizedBox.expand(
+                child: RaisedButton(
+                  color: Colors.white,
+                  onPressed: _recordVideo,
+                  child: Text(secondButtonText,
+                      style: TextStyle(
+                          fontSize: textSize, color: Colors.blueGrey)),
+                ),
+              )),
+              flex: 1,
+            )
+          ],
         ),
       ),
-    );
+    ));
+  }
+
+  void _takePhoto() async {
+    File recodedImage = await ImagePicker.pickImage(source: ImageSource.camera);
+    if (recodedImage != null && recodedImage.path != null) {
+      setState(() {
+        firstButtonText = 'saving in progress...';
+      });
+      await CameraContentSaver.saveImage(recodedImage.path);
+      setState(() {
+        firstButtonText = 'image saved!';
+      });
+    }
+  }
+
+  void _recordVideo() async {
+    File recodedVideo = await ImagePicker.pickVideo(source: ImageSource.camera);
+    if (recodedVideo != null && recodedVideo.path != null) {
+      setState(() {
+        secondButtonText = 'saving in progress...';
+      });
+      await CameraContentSaver.saveVideo(recodedVideo.path);
+      setState(() {
+        secondButtonText = 'video saved!';
+      });
+    }
   }
 }
