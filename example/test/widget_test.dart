11eb4f18013218d3dbc26e22f978982c73876f3b
jelenalecic <jelena.carnegie@gmail.com>
2019-08-01 15:38:28 +0200

    added author

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:gallery_saver_example/main.dart';
+import '../lib/main.dart';
 
 void main() {
   testWidgets('Verify UI is displayed', (WidgetTester tester) async {
